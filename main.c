973d3dd3d4eb8fc6ab594fbd22e696a4c5a445b3
Pablo <pablodonato173@gmail.com>
2026-08-18 16:47:36 -0300

    será

--- main.c
+++ main.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    printf("This is a sample C program.\n");
-    printf("This line will never be executed.\n");
-    printf("This line will also never be executed.\n");
-    //SOCOOOOOOOOOOOOOOOOOOOOOOOOORRO
+    printf("perchance\n");    return 0;
 }
