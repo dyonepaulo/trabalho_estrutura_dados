e5da2e1f2a37aebea2abaeaa379f8ec1ce0d1e29
Pablo <pablodonato173@gmail.com>
2026-08-21 20:35:56 -0300

    minor fixes

--- produto.c
+++ produto.c
@@ -128,18 +128,18 @@ int remover_produto (produto **estoque_produtos, int *tamanho_estoque){
 
             (*tamanho_estoque)--;
 
-            //if(*tamanho_estoque>5){
+            if(*tamanho_estoque>5){
                 produto *ptr_temp=realloc(*estoque_produtos, *tamanho_estoque*sizeof(produto));
 
                 if(ptr_temp==NULL && *tamanho_estoque > 0){             //se não verificar se o estoque é maior que 0, caso seja 0 vai retornar NULL e dar erro
                  printf("\nErro ao realocar estoque.\n");
                  return 1;
                 }
-           // }
+                *(estoque_produtos)=ptr_temp;
+            }
 
             printf("\nO produto %s foi removido com sucesso.\n", temp_nome);
 
-            *(estoque_produtos)=ptr_temp;
             return 0;
         }
     }
